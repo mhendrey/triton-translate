046f81a7ef30fc578dea50e7ae3832e1f4ac077a
Matthew Hendrey <matthew.hendrey@gmail.com>
2024-09-10 19:51:02 -0400

    Refactored validate.py to clean up the code and make a better output table
    New results will be added when they are done running.

--- model-repository/translate/validate.py
+++ model-repository/translate/validate.py
@@ -71,6 +71,8 @@ def test_pair(src, tgt, use_src: bool = True, translation_model: str = "seamless
         flores_src = "zho_Hant"
     elif src == "cmn":
         flores_src = "zho_Hans"
+    elif src == "sat_Beng":
+        flores_src = "sat_Olck"
     else:
         flores_src = src
     src_sentence = [
@@ -85,6 +87,8 @@ def test_pair(src, tgt, use_src: bool = True, translation_model: str = "seamless
         flores_tgt = "zho_Hant"
     elif tgt == "cmn":
         flores_tgt = "zho_Hans"
+    elif tgt == "sat_Beng":
+        flores_tgt = "sat_Olck"
     else:
         flores_tgt = tgt
     tgt_sentence = [
@@ -125,295 +129,153 @@ def test_pair(src, tgt, use_src: bool = True, translation_model: str = "seamless
 
 def main():
     # These are the valid language codes in SeamlessM4Tv2Large
-    # Notice that zho is not here. Need to rename that to cmn
-    language_codes_seamless = [
-        "afr",
-        "amh",
-        "arb",
-        "ary",
-        "arz",
-        "asm",
-        "azj",
-        "bel",
-        "ben",
-        "bos",
-        "bul",
-        "cat",
-        "ceb",
-        "ces",
-        "ckb",
-        "cmn",
-        "cmn_Hant",
-        "cym",
-        "dan",
-        "deu",
-        "ell",
+    # Given as (SeamlessM4T, NLLB)
+    language_codes = [
+        ("afr", "afr_Latn"),
+        ("amh", "amh_Ethi"),
+        ("arb", "arb_Arab"),
+        ("ary", "ary_Arab"),
+        ("arz", "arz_Arab"),
+        ("asm", "asm_Beng"),
+        ("azj", "azj_Latn"),
+        ("bel", "bel_Cyrl"),
+        ("ben", "ben_Beng"),
+        ("bos", "bos_Latn"),
+        ("bul", "bul_Cyrl"),
+        ("cat", "cat_Latn"),
+        ("ceb", "ceb_Latn"),
+        ("ces", "ces_Latn"),
+        ("ckb", "ckb_Arab"),
+        ("cmn", "zho_Hans"),
+        ("cmn_Hant", "zho_Hant"),
+        ("cym", "cym_Latn"),
+        ("dan", "dan_Latn"),
+        ("deu", "deu_Latn"),
+        ("ell", "ell_Grek"),
         # "eng", # Skip English
-        "est",
-        "eus",
-        "fin",
-        "fra",
-        "fuv",
-        "gaz",
-        "gle",
-        "glg",
-        "guj",
-        "heb",
-        "hin",
-        "hrv",
-        "hun",
-        "hye",
-        "ibo",
-        "ind",
-        "isl",
-        "ita",
-        "jav",
-        "jpn",
-        "kan",
-        "kat",
-        "kaz",
-        "khk",
-        "khm",
-        "kir",
-        "kor",
-        "lao",
-        "lit",
-        "lug",
-        "luo",
-        "lvs",
-        "mai",
-        "mal",
-        "mar",
-        "mkd",
-        "mlt",
-        "mni",
-        "mya",
-        "nld",
-        "nno",
-        "nob",
-        "npi",
-        "nya",
-        "ory",
-        "pan",
-        "pbt",
-        "pes",
-        "pol",
-        "por",
-        "ron",
-        "rus",
-        "sat",
-        "slk",
-        "slv",
-        "sna",
-        "snd",
-        "som",
-        "spa",
-        "srp",
-        "swe",
-        "swh",
-        "tam",
-        "tel",
-        "tgk",
-        "tgl",
-        "tha",
-        "tur",
-        "ukr",
-        "urd",
-        "uzn",
-        "vie",
-        "yor",
-        "yue",
-        "zsm",
-        "zul",
+        ("est", "est_Latn"),
+        ("eus", "eus_Latn"),
+        ("fin", "fin_Latn"),
+        ("fra", "fra_Latn"),
+        ("fuv", "fuv_Latn"),
+        ("gaz", "gaz_Latn"),
+        ("gle", "gle_Latn"),
+        ("glg", "glg_Latn"),
+        ("guj", "guj_Gujr"),
+        ("heb", "heb_Hebr"),
+        ("hin", "hin_Deva"),
+        ("hrv", "hrv_Latn"),
+        ("hun", "hun_Latn"),
+        ("hye", "hye_Armn"),
+        ("ibo", "ibo_Latn"),
+        ("ind", "ind_Latn"),
+        ("isl", "isl_Latn"),
+        ("ita", "ita_Latn"),
+        ("jav", "jav_Latn"),
+        ("jpn", "jpn_Jpan"),
+        ("kan", "kan_Knda"),
+        ("kat", "kat_Geor"),
+        ("kaz", "kaz_Cyrl"),
+        ("khk", "khk_Cyrl"),
+        ("khm", "khm_Khmr"),
+        ("kir", "kir_Cyrl"),
+        ("kor", "kor_Hang"),
+        ("lao", "lao_Laoo"),
+        ("lit", "lit_Latn"),
+        ("lug", "lug_Latn"),
+        ("luo", "luo_Latn"),
+        ("lvs", "lvs_Latn"),
+        ("mai", "mai_Deva"),
+        ("mal", "mal_Mlym"),
+        ("mar", "mar_Deva"),
+        ("mkd", "mkd_Cyrl"),
+        ("mlt", "mlt_Latn"),
+        ("mni", "mni_Beng"),
+        ("mya", "mya_Mymr"),
+        ("nld", "nld_Latn"),
+        ("nno", "nno_Latn"),
+        ("nob", "nob_Latn"),
+        ("npi", "npi_Deva"),
+        ("nya", "nya_Latn"),
+        ("ory", "ory_Orya"),
+        ("pan", "pan_Guru"),
+        ("pbt", "pbt_Arab"),
+        ("pes", "pes_Arab"),
+        ("pol", "pol_Latn"),
+        ("por", "por_Latn"),
+        ("ron", "ron_Latn"),
+        ("rus", "rus_Cyrl"),
+        ("sat", "sat_Beng"), # Flores has sat_Olck, but not sat_Beng
+        ("slk", "slk_Latn"),
+        ("slv", "slv_Latn"),
+        ("sna", "sna_Latn"),
+        ("snd", "snd_Arab"),
+        ("som", "som_Latn"),
+        ("spa", "spa_Latn"),
+        ("srp", "srp_Cyrl"),
+        ("swe", "swe_Latn"),
+        ("swh", "swh_Latn"),
+        ("tam", "tam_Taml"),
+        ("tel", "tel_Telu"),
+        ("tgk", "tgk_Cyrl"),
+        ("tgl", "tgl_Latn"),
+        ("tha", "tha_Thai"),
+        ("tur", "tur_Latn"),
+        ("ukr", "ukr_Cyrl"),
+        ("urd", "urd_Arab"),
+        ("uzn", "uzn_Latn"),
+        ("vie", "vie_Latn"),
+        ("yor", "yor_Latn"),
+        ("yue", "yue_Hant"),
+        ("zsm", "zsm_Latn"),
+        ("zul", "zul_Latn"),
     ]
 
-    language_codes_nllb = [
-        "afr_Latn",
-        "amh_Ethi",
-        "arb_Arab",
-        "ary_Arab",
-        "arz_Arab",
-        "asm_Beng",
-        "azj_Latn",
-        "bel_Cyrl",
-        "ben_Beng",
-        "bos_Latn",
-        "bul_Cyrl",
-        "cat_Latn",
-        "ceb_Latn",
-        "ces_Latn",
-        "ckb_Arab",
-        "cym_Latn",
-        "dan_Latn",
-        "deu_Latn",
-        "ell_Grek",
-        # "eng_Latn", # Skip English
-        "est_Latn",
-        "eus_Latn",
-        "fin_Latn",
-        "fra_Latn",
-        "fuv_Latn",
-        "gaz_Latn",
-        "gle_Latn",
-        "glg_Latn",
-        "guj_Gujr",
-        "heb_Hebr",
-        "hin_Deva",
-        "hrv_Latn",
-        "hun_Latn",
-        "hye_Armn",
-        "ibo_Latn",
-        "ind_Latn",
-        "isl_Latn",
-        "ita_Latn",
-        "jav_Latn",
-        "jpn_Jpan",
-        "kan_Knda",
-        "kat_Geor",
-        "kaz_Cyrl",
-        "khk_Cyrl",
-        "khm_Khmr",
-        "kir_Cyrl",
-        "kor_Hang",
-        "lao_Laoo",
-        "lit_Latn",
-        "lug_Latn",
-        "luo_Latn",
-        "lvs_Latn",
-        "mai_Deva",
-        "mal_Mlym",
-        "mar_Deva",
-        "mkd_Cyrl",
-        "mlt_Latn",
-        "mni_Beng",
-        "mya_Mymr",
-        "nld_Latn",
-        "nno_Latn",
-        "nob_Latn",
-        "npi_Deva",
-        "nya_Latn",
-        "ory_Orya",
-        "pan_Guru",
-        "pbt_Arab",
-        "pes_Arab",
-        "pol_Latn",
-        "por_Latn",
-        "ron_Latn",
-        "rus_Cyrl",
-        # "sat_Beng", # Flores has sat_Olck, but not sat_Beng
-        "slk_Latn",
-        "slv_Latn",
-        "sna_Latn",
-        "snd_Arab",
-        "som_Latn",
-        "spa_Latn",
-        "srp_Cyrl",
-        "swe_Latn",
-        "swh_Latn",
-        "tam_Taml",
-        "tel_Telu",
-        "tgk_Cyrl",
-        "tgl_Latn",
-        "tha_Thai",
-        "tur_Latn",
-        "ukr_Cyrl",
-        "urd_Arab",
-        "uzn_Latn",
-        "vie_Latn",
-        "yor_Latn",
-        "yue_Hant",
-        "zho_Hans",
-        "zho_Hant",
-        "zsm_Latn",
-        "zul_Latn",
-    ]
-
-    errors = []
+    errors = {}
     chrf2 = []
-    errors_no_src = []
-    chrf2_no_src = []
-    start_time = datetime.now()
-    print(f"| Language | SeamlessM4T chrF2++ w/ src_lang | SeamlessM4T chrF2++ no src_lang |")
-    print(f"| :------: | :-----------------------------: | :-----------------------------: |")
-    for i, src in enumerate(language_codes_seamless):
-        print(f"| {src} |", end="", flush=True)
-        try:
-            triton_score, errors_dict = test_pair(src, "eng", translation_model="seamlessm4t_text2text")
-        except Exception as exc:
-            errors.append(f"{src} threw {exc}\n")
-        else:
-            print(f" {triton_score:.1f} |", end="", flush=True)
-            chrf2.append(triton_score)
-            errors.append(errors_dict)
+    print("| SeamlessM4T Language ", end="")
+    print("| SeamlessM4T chrF2++ w/ src_lang ", end="")
+    print("| SeamlessM4T chrF2++ no src_lang ", end="")
+    print("| NLLB Language ", end="")
+    print("| NLLB chrF2++ w/ src_lang ", end="")
+    print("| NLLB chrF2++ no src_lang |")
+    print("| :--: | :--: | :--: | :--: | :--: | :--: |", flush=True)
+    for language_code in language_codes:
+        errors_lang = []
+        chrf2_lang = []
+        for i, src in enumerate(language_code):
+            if i == 0: # SeamlessM4T
+                tgt = "eng"
+                translation_model = "seamlessm4t"
+            else: # NLLB
+                tgt = "eng_Latn"
+                translation_model = "nllb"
+            print(f"| {src} ", end="", flush=True)
+            for use_src in enumerate([True, False]):
+                try:
+                    triton_score, errors_dict = test_pair(
+                        src, tgt, use_src=use_src, translation_model=translation_model
+                    )
+                except Exception as exc:
+                    errors_lang.append(f"{src} threw {exc}\n")
+                else:
+                    print(f"| {triton_score:.1f} ", end="", flush=True)
+                    chrf2_lang.append(triton_score)
+                    errors_lang.append(errors_dict)
+        print("|", flush=True)
+        errors[language_code] = errors_lang
+        chrf2.append(chrf2_lang)
 
-        try:
-            triton_score, errors_dict = test_pair(src, "eng", use_src=False, translation_model="seamlessm4t_text2text")
-        except Exception as exc:
-            errors_no_src.append(f"{src} threw {exc}\n")
-        else:
-            print(f" {triton_score:.1f} |", flush=True)
-            chrf2_no_src.append(triton_score)
-            errors_no_src.append(errors_dict)
-
-    end_time = datetime.now()
-
-    mean_score = sum(chrf2) / len(chrf2)
-    mean_no_score = sum(chrf2_no_src) / len(chrf2_no_src)
-    print(f"| **Mean** | **{mean_score:.2f}** | **{mean_no_score:.2f}** |")
-    print(f"\nTime taken: {end_time - start_time}")
-
-    print(f"Errors when using src_lang in SeamlessM4T")
-    for errors_dict in errors:
-        if errors_dict:
-            pprint(errors_dict)
-    print("\n\nErrors when no src_lang in SeamlessM4T")
-    for errors_dict in errors_no_src:
-        if errors_dict:
-            pprint(errors_dict)
-
-    errors = []
-    chrf2 = []
-    errors_no_src = []
-    chrf2_no_src = []
-    start_time = datetime.now()
-    print(f"| Language | NLLB chrF2++ w/ src_lang | NLLB chrF2++ no src_lang |")
-    print(f"| :------: | :----------------------: | :----------------------: |")
-    for i, src in enumerate(language_codes_nllb):
-        print(f"| {src} |", end="", flush=True)
-        try:
-            triton_score, errors_dict = test_pair(src, "eng_Latn", translation_model="nllb_200_distilled_600M")
-        except Exception as exc:
-            errors.append(f"{src} threw {exc}\n")
-        else:
-            print(f" {triton_score:.1f} |", end="", flush=True)
-            chrf2.append(triton_score)
-            errors.append(errors_dict)
-
-        try:
-            triton_score, errors_dict = test_pair(src, "eng_Latn", use_src=False, translation_model="nllb_200_distilled_600M")
-        except Exception as exc:
-            errors_no_src.append(f"{src} threw {exc}\n")
-        else:
-            print(f" {triton_score:.1f} |", flush=True)
-            chrf2_no_src.append(triton_score)
-            errors_no_src.append(errors_dict)
+    chrf2_mean = np.array(chrf2).mean(axis=0)
+    print(f"| **Mean** | **{chrf2_mean[0]:.2f}** | **{chrf2_mean[1]:.2f}** ", end="")
+    print(f"| | **{chrf2_mean[2]:.2f}** | **{chrf2_mean[3]:.2f}** |")
 
 
-    end_time = datetime.now()
-
-    mean_score = sum(chrf2) / len(chrf2)
-    mean_no_score = sum(chrf2_no_src) / len(chrf2_no_src)
-    print(f"| **Mean** | **{mean_score:.2f}** | **{mean_no_score:.2f}** |")
-    print(f"\nTime taken: {end_time - start_time}")
-
-    print(f"Errors when using src_lang in NLLB")
-    for errors_dict in errors:
-        if errors_dict:
-            pprint(errors_dict)
-    print("\n\nErrors when no src_lang in NLLB")
-    for errors_dict in errors_no_src:
-        if errors_dict:
-            pprint(errors_dict)
+    for language_code, error_lang in errors.items():
+        for error_dict in error_lang:
+            if len(error_dict) > 0:
+                print(f"{language_code=}")
+                pprint(error_dict)
 
 
 
